e429f378fff99cff0fe54f3af6fdacdd041a2630
ayano <ayano@pactreon.com>
2024-11-10 05:03:07 -0500

    highest and lowest blocks

--- challenge-1/src/main.rs
+++ challenge-1/src/main.rs
@@ -3,6 +3,7 @@ use futures::StreamExt;
 use subxt::{client::OnlineClient, lightclient::LightClient, PolkadotConfig};
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::collections::HashMap;
 
 // Generate an interface that we can use from the node's metadata.
 #[subxt::subxt(runtime_metadata_path = "artifacts/polkadot_metadata_small.scale")]
@@ -20,29 +21,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Instantiate a light client with the Polkadot relay chain,
     // and connect it to Asset Hub, too.
-    // let (lightclient, polkadot_rpc) = LightClient::relay_chain(POLKADOT_SPEC)?;
-    // let asset_hub_rpc = lightclient.parachain(ASSET_HUB_SPEC)?;
-
     let (paseo_light_client, paseo_rpc) = LightClient::relay_chain(PASEO_SPEC)?;
     let pp_rpc = paseo_light_client.parachain(POLKADOT_PEOPLE_SPEC)?;
 
     // Create Subxt clients from these Smoldot backed RPC clients.
-    // let polkadot_api = OnlineClient::<PolkadotConfig>::from_rpc_client(polkadot_rpc).await?;
-    // let asset_hub_api = OnlineClient::<PolkadotConfig>::from_rpc_client(asset_hub_rpc).await?;
-
     let paseo_api = OnlineClient::<PolkadotConfig>::from_rpc_client(paseo_rpc).await?;
     let pp_api = OnlineClient::<PolkadotConfig>::from_rpc_client(pp_rpc).await?;
-
-    // let polkadot_sub = polkadot_api
-    //     .blocks()
-    //     .subscribe_finalized()
-    //     .await?
-    //     .map(|block| ("Polkadot", block));
-    // let parachain_sub = asset_hub_api
-    //     .blocks()
-    //     .subscribe_finalized()
-    //     .await?
-    //     .map(|block| ("AssetHub", block));
 
     let paseo_sub = paseo_api
         .blocks()
@@ -60,27 +44,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         paseo_sub, pp_sub,
     );
 
+    // Track the highest and lowest block numbers for each chain.
+    let mut highest_blocks: HashMap<&str, u32> = HashMap::new();
+    let mut lowest_blocks: HashMap<&str, u32> = HashMap::new();
+
     while let Some((chain, block)) = stream_combinator.next().await {
         let block = block?;
+        let block_number = block.number();
         println!(
             "📦 Chain {:?} | hash={:?} | height={:?}",
             chain,
             block.hash(),
-            block.number()
+            block_number
         );
 
         let log_data = format!(
             "Chain: {}, hash: {:?}, height: {}\n",
             chain,
             block.hash(),
-            block.number()
+            block_number
         );
 
         init_logs(&log_data)?;
 
         // TODO: `🍫 Intermediate` Finding the chain with highest block number.
+        highest_blocks
+            .entry(chain)
+            .and_modify(|highest| {
+                if block_number > *highest {
+                    *highest = block_number;
+                }
+            })
+            .or_insert(block_number);
+        println!("Current highest blocks: {:?}", highest_blocks);
 
-        // TODO: `🍫 Intermediate` Finding the chain with lowest block number.
+        lowest_blocks
+            .entry(chain)
+            .and_modify(|lowest| {
+                if block_number < *lowest {
+                    *lowest = block_number;
+                }
+            })
+            .or_insert(block_number);
+        println!("Current lowest blocks: {:?}", lowest_blocks);
 
         // TODO: `🔥 Advanced` Processing extrinsics of each block and aggregate the number of transactions made based on the pallet name. Store the data in the log file named `pallets.txt`.
 
