0f1a2b601202a075dcabe78b7ab041ff14aef55e
Riki <ricardocr987@gmail.com>
2024-11-11 15:36:53 +0700

    challange 1

--- challenge-1/src/main.rs
+++ challenge-1/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(missing_docs)]
 use futures::StreamExt;
+use std::{fs::OpenOptions, io::Write, sync::{Arc, Mutex}};
 use subxt::{client::OnlineClient, lightclient::LightClient, PolkadotConfig};
+use tracing_subscriber;
 
 // Generate an interface that we can use from the node's metadata.
 #[subxt::subxt(runtime_metadata_path = "artifacts/polkadot_metadata_small.scale")]
@@ -12,55 +14,88 @@ const ASSET_HUB_SPEC: &str = include_str!("../artifacts/chain_specs/polkadot_ass
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // The lightclient logs are informative:
+    // Initialize logging for the light client.
     tracing_subscriber::fmt::init();
 
-    // Instantiate a light client with the Polkadot relay chain,
-    // and connect it to Asset Hub, too.
+    // Instantiate a light client with the Polkadot relay chain and connect it to Asset Hub.
     let (lightclient, polkadot_rpc) = LightClient::relay_chain(POLKADOT_SPEC)?;
     let asset_hub_rpc = lightclient.parachain(ASSET_HUB_SPEC)?;
 
-    // TODO: `🍭 Easy` Initialize RPCs to new relaychains and parachains.
-
-    // Create Subxt clients from these Smoldot backed RPC clients.
+    // Create Subxt clients from the Smoldot backed RPC clients.
     let polkadot_api = OnlineClient::<PolkadotConfig>::from_rpc_client(polkadot_rpc).await?;
     let asset_hub_api = OnlineClient::<PolkadotConfig>::from_rpc_client(asset_hub_rpc).await?;
 
-    // TODO: `🍭 Easy` Create Subxt clients from newly added Smoldot backed RPC clients.
+    // Subscribe to finalized blocks from both chains.
+    let polkadot_sub = polkadot_api.blocks().subscribe_finalized().await?;
+    let parachain_sub = asset_hub_api.blocks().subscribe_finalized().await?;
 
-    let polkadot_sub = polkadot_api
-        .blocks()
-        .subscribe_finalized()
-        .await?
-        .map(|block| ("Polkadot", block));
-    let parachain_sub = asset_hub_api
-        .blocks()
-        .subscribe_finalized()
-        .await?
-        .map(|block| ("AssetHub", block));
+    let mut stream_combinator = futures::stream::select(polkadot_sub.map(|block| ("Polkadot", block)), parachain_sub.map(|block| ("AssetHub", block)));
 
-    // TODO: `🍭 Easy` Fetch blocks from new chains using the added APIs.
+    // Prepare log files for blocks, pallets, and events.
+    let log_file = Arc::new(Mutex::new(OpenOptions::new().append(true).create(true).open("blocks_log.txt")?));
+    let pallet_file = Arc::new(Mutex::new(OpenOptions::new().append(true).create(true).open("pallets.txt")?));
+    let events_file = Arc::new(Mutex::new(OpenOptions::new().append(true).create(true).open("events.txt")?));
 
-    let mut stream_combinator = futures::stream::select(polkadot_sub, parachain_sub);
+    let mut highest_block = None;
+    let mut lowest_block = None;
+    
+    while let Some((chain, block_result)) = stream_combinator.next().await {
+        let block = block_result?;
+        let block_number = block.number();
+    
+        // Log block information.
+        let block_log = format!("📦 Chain: {:?} | Hash: {:?} | Height: {:?}\n", chain, block.hash(), block_number);
+        {
+            let mut file = log_file.lock().unwrap();
+            file.write_all(block_log.as_bytes())?;
+        }
+    
+        // Update highest and lowest block numbers.
+        highest_block = match highest_block {
+            Some((_, highest)) if block_number > highest => Some((chain, block_number)),
+            None => Some((chain, block_number)),
+            _ => highest_block,
+        };
+    
+        lowest_block = match lowest_block {
+            Some((_, lowest)) if block_number < lowest => Some((chain, block_number)),
+            None => Some((chain, block_number)),
+            _ => lowest_block,
+        };
+    
+        // Process extrinsics and log pallet names.
+        let extrinsics = block.extrinsics().await?;
+        let extrinsics_data: Vec<_> = extrinsics.iter()
+            .filter_map(|ext| {
+                match ext.pallet_name() {
+                    Ok(name) => Some(name.to_string()),
+                    Err(_) => None,
+                }
+            })
+            .collect();
 
-    while let Some((chain, block)) = stream_combinator.next().await {
-        let block = block?;
-        println!(
-            "📦 Chain {:?} | hash={:?} | height={:?}",
-            chain,
-            block.hash(),
-            block.number()
-        );
+        {
+            let mut file = pallet_file.lock().unwrap();
+            for pallet in extrinsics_data {
+                writeln!(file, "Chain: {:?} | Pallet: {:?}", chain, pallet)?;
+            }
+        }
 
-        // TODO: `🍫 Intermediate` Store the fetched block data to a log file.
+        let events: subxt::events::Events<PolkadotConfig> = block.events().await?;        
+        {
+            let mut file = events_file.lock().unwrap();
+            for event in events.iter() {
+                writeln!(file, "Chain: {:?} | Event: {:?}", chain, event?.pallet_name())?;
+            }
+        }
+    }
 
-        // TODO: `🍫 Intermediate` Finding the chain with highest block number.
-
-        // TODO: `🍫 Intermediate` Finding the chain with lowest block number.
-
-        // TODO: `🔥 Advanced` Processing extrinsics of each block and aggregate the number of transactions made based on the pallet name. Store the data in the log file named `pallets.txt`.
-
-        // TODO: `🔥 Advanced` Processing events emitted from each block and aggregate the number of events made based on the event name. Store the data in the log file named `events.txt`.
+    // Log the highest and lowest block information.
+    if let Some((chain, highest_block)) = highest_block {
+        println!("🚀 Highest Block: Chain: {:?} | Height: {:?}", chain, highest_block);
+    }
+    if let Some((chain, lowest_block)) = lowest_block {
+        println!("🐢 Lowest Block: Chain: {:?} | Height: {:?}", chain, lowest_block);
     }
 
     Ok(())
